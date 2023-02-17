f0e5926f9631d2a94aa51140cb4c71102e911aa3
mohammed-majeed <mohammed.y.majeed@gmail.com>
2023-02-17 18:24:04 +0100

    hi2

--- EDDA_assignment_1.R
+++ EDDA_assignment_1.R
@@ -1,20 +1,6 @@
-my_data <- read.delim(file.choose(),header = TRUE)
-data = my_data$birthweight
-qqnorm(data, main = "Normal Q-Q Plot of Birthweight")
-
-hist(x = data,breaks=15,main = "Histogram of Birthweights",
-     xlab = " Birthweight ",freq = FALSE)
-
-lines(x= density(x= data), col=" red ")
-
-boxplot (data , main =" Boxplot of Birthweight ",
-          xlab =" Birthweight ")#horizontal = TRUE)
-
-
-
-
 # Read data from file and extract birthweight column
 data = read.table("birthweight.txt", header=TRUE)
+
 birthweight = data$birthweight
 
 # Check for normality and compute Shapiro-Wilk test p-value
