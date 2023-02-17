9e2ac2ff5db87c781df9210eda6f034795bbd7bb
Mohammed-majeed <113516250+Mohammed-majeed@users.noreply.github.com>
2023-02-17 13:12:09 +0100

    Update EDDA_assignment_1.R

--- EDDA_assignment_1.R
+++ EDDA_assignment_1.R
@@ -1,3 +1,11 @@
 my_data <- read.delim(file.choose(),header = TRUE)
 data = my_data$birthweight
-qqnorm(my_data, main = "Normal Q-Q Plot")
+qqnorm(data, main = "Normal Q-Q Plot of Birthweight")
+
+hist(x = data,breaks=15,main = "Histogram of Birthweights",
+     xlab = " Birthweight ",freq = FALSE)
+
+lines(x= density(x= data), col=" red ")
+
+boxplot (data , main =" Boxplot of Birthweight ",
+          xlab =" Birthweight ")#horizontal = TRUE)
