95374e296692697719602868fb838b93811a27e2
mohammed-majeed <mohammed.y.majeed@gmail.com>
2023-02-24 11:39:36 +0100

    hi11

--- EDDA_assignment_(1)_ex_4.R
+++ EDDA_assignment_(1)_ex_4.R
@@ -3,57 +3,43 @@ library(MASS)
 data(npk)
 npk
 ######----- A -------#####
-# Create a data frame to store the randomized treatments
-treatments <- data.frame(yield = numeric(24), block = rep(1:6, each = 4),
-                         N = numeric(24), P = numeric(24), K = numeric(24))
-
-# Randomly assign treatments to the plots
-
-for(i in 1:6){
-  ind <- which(treatments$block == i)
-  treatments[ind, c("N", "P", "K")] <- t(replicate(2, sample(c(0, 1), 3,replace = TRUE)))
-}
-
-# View the randomized treatments
-head(treatments)
+#Create a matrix of random plots
+random_plots_matrix <- cbind(rep(1:24),rep(1:6, each = 4),
+                             replicate(3, c(replicate(6, sample(c(1,1,0,0))))))
+#Convert the matrix to a data frame
+data_frame <- data.frame(random_plots_matrix)
+#Set column names
+header <- c("plot", "block", "N", "P", "K")
+colnames(data_frame) <- header
+#View the resulting data frame
+head(data_frame)
 
 
 ######----- B -------#####
 
 # Subset the data to only include nitrogen treatment
 npk_nitrogen  <- npk[npk$N == 1,]
-
 # Calculate the average yield per block for nitrogen treatment
 nitrogen_avg_yield  <- aggregate(yield ~ block, data = npk_nitrogen, FUN = mean)
-
 # Subset the data to only include plots without nitrogen treatment
 npk_no_nitrogen  <- npk[npk$N == 0,]
-
 # Calculate the average yield per block for plots without nitrogen treatment
 no_nitrogen_avg_yield  <- aggregate(yield ~ block, data = npk_no_nitrogen, FUN = mean)
-
-
-
 # Make a boxplot comparing the average yield per block for nitrogen and no nitrogen treatments
 boxplot(no_nitrogen_avg_yield$yield, nitrogen_avg_yield$yield, 
         names = c("No Nitrogen Treatment", "Nitrogen Treatment"), ylab = "yield")
-
 #legend("topleft", legend = c("Nitrogen Treatment", "No Nitrogen Treatment"))
 
-ddf = structure(list(no_nitrogen_avg_yield$yield,nitrogen_avg_yield$yield),
-                .Names = c("No Nitrogen Treatment", "Nitrogen Treatment"), class = "data.frame")
 
-barplot(c(no_nitrogen_avg_yield$yield,nitrogen_avg_yield$yield),
-        col = c("#1b98e0", "#353436"),beside = TRUE, xlab="blocks",ylab = "yield",
-        ylim=c(0,90))
-axis(side = 1, at = 1.5, labels =( "1"))
-axis(side = 1, at = 4, labels =( "2"))
-axis(side = 1, at = 6.5, labels =( "3"))
-axis(side = 1, at = 9, labels =( "4"))
-axis(side = 1, at = 11.5, labels =( "5"))
-axis(side = 1, at = 13.5, labels =( "6"))
-legend("topright", legend = c("Nitrogen Treatment", "No Nitrogen Treatment"),
-       fill = c("#1b98e0", "#353436"))
+avg_yield <- apply(cbind(nitrogen_avg_yield[[2]], no_nitrogen_avg_yield[[2]]),1,function(x) unname(unlist(x)))
+rownames(avg_yield)<- c("nitrogen_avg_yield","no_nitrogen_avg_yield")
+
+barplot(avg_yield, col = c("#1b98e0", "#353436"),beside = TRUE,
+        xlab="blocks",ylab = "yield", ylim=c(0,90),
+        names.arg=c("1", "2", "3", "4", "5", "6"))
+
+legend("topleft", legend = c("Nitrogen Treatment", "No Nitrogen Treatment"), 
+       fill =c("#1b98e0", "#353436"))
 
 
 ######----- D -------#####
